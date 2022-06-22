2ef8097fde75810a5dd248129c3683143e035c3c
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-06-22 12:39:23 +0200

    javascript povezivanje sa stranicom

--- shared.js
+++ shared.js
@@ -1,6 +1,25 @@
 var backdrop = document.querySelector('.backdrop'); /*ime var-a (backdrop) ne mora biti ime elementa ili selektora kojeg zelimo da pozovemo*/
-var selectPlanButtons = document.querySelectorAll(.plan button);
-console.dir(selectPlanButtons);
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var noDugme = document.querySelector('.modal button');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
+
+backdrop.addEventListener('click', function() {   /*ovo omogucava da se klikne bilo gde na stranici da bi se prozor 'yes no' uklonio'*/
+        modal.style.display = 'none';
+        backdrop.style.display = 'none';
+    });
+
+noDugme.addEventListener('click', function() {  /*ovo omogucava da se prozor 'yes no' ukloni kada se klikne na 'no'*/
+        modal.style.display = 'none';
+        backdrop.style.display = 'none';
+    });
+
 
 
 
