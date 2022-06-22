d46d1778367d474b4017175bddcab1496ec64be1
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-06-22 10:37:25 +0200

    selektovanje svih button-a koji se nalaze u okviru plan-a

--- shared.js
+++ shared.js
@@ -1,3 +1,8 @@
 var backdrop = document.querySelector('.backdrop'); /*ime var-a (backdrop) ne mora biti ime elementa ili selektora kojeg zelimo da pozovemo*/
+var selectPlanButtons = document.querySelectorAll(.plan button);
 
-console.dir(backdrop);
+
+
+
+
+/*console.dir(backdrop);*/
