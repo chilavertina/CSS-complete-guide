a6c3c1b24c64bc6c5b452e9fe5661ef0ac0d9620
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-06-22 10:40:33 +0200

    console.dir koji nam omogucava uvid u sve button-e koje smo selektovali

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
 var backdrop = document.querySelector('.backdrop'); /*ime var-a (backdrop) ne mora biti ime elementa ili selektora kojeg zelimo da pozovemo*/
 var selectPlanButtons = document.querySelectorAll(.plan button);
+console.dir(selectPlanButtons);
 
 
 
